e569296a2c65ca5ef9c021b04bb37179a6794854
Pritam Nikam <pritam.nikam@gmail.com>
2024-06-01 16:29:18 +0530

    first commit

---
+++ src/main/java/com/spring/firstjobapp/job/JobService.java
@@ -0,0 +1,13 @@
+package com.spring.firstjobapp.job;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface JobService {
+    List<Job> findAll();
+    void createJob(Job job);
+    Job getJobById(Long id);
+    boolean deleteJobById(Long id);
+    boolean updateJob(Long id, Job updatedJob);
+}
